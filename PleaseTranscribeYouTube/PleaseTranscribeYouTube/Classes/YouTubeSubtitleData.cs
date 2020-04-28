ef3484fbbd6799c5bcdef1402108645055c64b10
Jubin Park <jubin-park@naver.com>
2020-04-29 00:54:09 +0900

    Add YouTubeSubtitleData.cs

---
+++ PleaseTranscribeYouTube/PleaseTranscribeYouTube/Classes/YouTubeSubtitleData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PleaseTranscribeYouTube.Classes
+{
+    class YouTubeSubtitleData
+    {
+        private string mText;
+        private double mStartTime;
+        private double mDurationTime;
+
+        public YouTubeSubtitleData(string text, string start, string duration)
+        {
+            mText = text;
+            mStartTime = double.Parse(start);
+            mDurationTime = double.Parse(duration);
+        }
+    }
+}
