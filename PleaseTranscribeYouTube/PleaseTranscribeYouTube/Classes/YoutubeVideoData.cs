d6948aaf22b02bf77df27434af00f0dd8cfd84d8
Jubin Park <jubin-park@naver.com>
2020-04-29 00:54:30 +0900

    Add YouTubeVideoData.cs

---
+++ PleaseTranscribeYouTube/PleaseTranscribeYouTube/Classes/YoutubeVideoData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PleaseTranscribeYouTube.Classes
+{
+    class YouTubeVideoData
+    {
+        private string mID;
+        private string mLanguage;
+        private List<YouTubeSubtitleData> mSubtitleDatas = new List<YouTubeSubtitleData>();
+
+        public YouTubeVideoData(string id)
+        {
+            mID = id;
+        }
+    }
+}
