717e5a4a1ff024252e44eba75dd650708561064d
Jubin Park <jubin-park@naver.com>
2020-06-05 21:36:51 +0900

    Add MultiConverterMarkupExtension

---
+++ ScripTube/ScripTube/Utils/MultiConverterMarkupExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace ScripTube.Utils
+{
+    public abstract class MultiConverterMarkupExtension<T> : MarkupExtension, IMultiValueConverter
+        where T : class, new()
+    {
+        private static Lazy<T> mConverter = new Lazy<T>(() => new T());
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return mConverter.Value;
+        }
+
+        public abstract object Convert(object[] values, Type targetType, object parameter, CultureInfo culture);
+        public abstract object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture);
+    }
+}
