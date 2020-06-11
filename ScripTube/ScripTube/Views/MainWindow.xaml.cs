32258671f79fff26fc1599355b9d5de5404049c3
Jubin Park <jubin-park@naver.com>
2020-06-12 03:04:14 +0900

    SeekTo When Doubleclicking on BookmarkItem

--- ScripTube/ScripTube/Views/MainWindow.xaml.cs
+++ ScripTube/ScripTube/Views/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using ScripTube.Models.Bookmark;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace ScripTube.Views
 {
@@ -7,6 +10,18 @@ namespace ScripTube.Views
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        // https://stackoverflow.com/questions/1035023/firing-a-double-click-event-from-a-wpf-listview-item-using-mvvm
+        private void xListViewBookmarkItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var listViewItem = sender as ListViewItem;
+            var bookmarkItem = (BookmarkItem)listViewItem.Content;
+
+            if (bookmarkItem != null)
+            {
+                xVideoPlayer.SeekTo(bookmarkItem.Seconds);
+            }
         }
     }
 }
